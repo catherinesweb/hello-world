2e67decff8934b73c9d21104cf933bb1fdc2a65e
cehillhyman <47611047+cehillhyman@users.noreply.github.com>
2021-02-02 21:03:07 -0500

    Delete TridentTech/2020 directory

--- TridentTech/2020/00-Spring/IST-239/Assignment2/myFavMovie.js
+++
@@ -1,8 +0,0 @@
-//Catherine Hill Hyman
-
-document.write("What We Do in the Shadows is a New Zealand indie comedy about four vampires who live together. ");
-document.write("The movie follows the vampires in a document-style filming similar to The Office as they navigate the modern world in human society. ");
-document.write("One night, the oldest vampire creates a new vampire. He's kind of annoying and makes mistakes like telling humans that he is a vampire and eating human food. ");
-document.write("However, the older vampires all love his friend, Stew! He teaches them how to use modern computers and phones, and helps them get invited into clubs. ");
-document.write("Complications arise when Stew's vampire friends introduce him to other vampires and supernatural creatures. ");
-document.write("They eventually run into werewolves (their sworn enemies) with Stew tagging along, resulting in even more modern supernatural shenanigans.");
